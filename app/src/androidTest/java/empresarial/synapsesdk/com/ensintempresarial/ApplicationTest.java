1cf584204e1b3711fa1404690c44957f90178dc9
Garyfimo <garyfimo@gmail.com>
2014-10-16 08:36:37 -0500

    first commit

---
+++ app/src/androidTest/java/empresarial/synapsesdk/com/ensintempresarial/ApplicationTest.java
@@ -0,0 +1,13 @@
+package empresarial.synapsesdk.com.ensintempresarial;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
