38aa3ba817edd396700abf664ed3dbed63d86ddd
Garyfimo <garyfimo@gmail.com>
2014-11-17 23:50:28 -0500

    ultimos retoques

--- app/src/main/java/empresarial/synapsesdk/com/activity/RecursosActivity.java
+++ app/src/main/java/empresarial/synapsesdk/com/activity/RecursosActivity.java
@@ -54,20 +54,13 @@ public class RecursosActivity extends BaseActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.recursos, menu);
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.progress, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
         return super.onOptionsItemSelected(item);
     }
 
